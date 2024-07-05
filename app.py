719c9630e98dbe8440dff5b210b2ecbb10973528
Chime <chimeobioha@gmail.com>
2024-07-05 09:05:30 +0100

    ADD new route: hello_visitor2, to try out the solution that allows request.remote_addr deliver dynamic IP by configuring ProxyFix for app.wsgi_app AND also add vercel.json to try deployment to vercel

--- app.py
+++ app.py
@@ -9,26 +9,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['APP_SECRET_KEY']
 
 
-@app.route("/")
-def home():
-    return Markup(
-        "<h1>Hello, Welcome!</h1> <br>"
-        "<h3>This is HNG11 Stage One Task by Chimeziri Obioha.</h3> <br>"
-        f"<h3>Visit <u><em>{request.base_url}api/hello?visitor_name='YOUR NAME'</em></u> for take-away greeting</h3>"
-    )
-
-
-@app.route("/api/hello", methods=['GET'])
-def hello_visitor():
-
-    visitor_name = request.args.get('visitor_name').replace('"', '').replace("'", '')
-
-    # Get Visitor's IP Address
-    if os.environ.get('APP_IN_PRODUCTION'):
-        visitor_ip = str(request.environ.get(
-            'HTTP_X_FORWARDED_FOR')) or str(request.environ.get('REMOTE_ADDR'))
-    else:
-        visitor_ip = request.remote_addr
+def get_json_result(req, visitor_ip):
+    """
+    Function that generates and returns the required json response
+    """
+    # Moved the code which previously lived inside `hello_visitor` here, 
+    # to achieve DRY code, because same code is now used in both `hello_visitor` 
+    # and the newly added function: `hello_visitor2`
+    
+    visitor_name = req.args.get('visitor_name').replace('"', '').replace("'", '')
 
     # get gealocation info for IP address
     loc_info = requests.get(f'https://ipapi.co/{visitor_ip}/json/', timeout=1200).json()
@@ -54,14 +43,56 @@ def hello_visitor():
     )
 
 
+@app.route("/")
+def home():
+    return Markup(
+        "<h1>Hello, Welcome!</h1> <br>"
+        "<h3>This is HNG11 Stage One Task by Chimeziri Obioha.</h3> <br>"
+        f"<h3>Visit <u><em>{request.base_url}api/hello?visitor_name='YOUR NAME'</em></u> for take-away greeting</h3>"
+    )
+
+
+@app.route("/api/hello", methods=['GET'])
+def hello_visitor():
+    """
+    MAIN ANSWER TO THE `HNG11-STAGE-1-BACKEND-TASK`
+    """
+    # Get Visitor's IP Address
+    if os.environ.get('APP_IN_PRODUCTION'):
+        visitor_ip = str(request.environ.get(
+            'HTTP_X_FORWARDED_FOR')) or str(request.environ.get('REMOTE_ADDR'))
+    else:
+        visitor_ip = request.remote_addr
+
+    return get_json_result(request, visitor_ip)
+
+
 @app.route("/api/test-production/", methods=['GET'])
 def test_production():
     """
-    Just to quick way to test out the production version when in localhost
+    Just a quick way to test out the production version when in localhost
     """
     prod_url = "https://chimeziri-obioha-hng11-stage1-task.cleverapps.io/api/hello?visitor_name='Chimeziri Obioha'"
 
     return jsonify(requests.get(prod_url, timeout=1200).json())
+
+
+@app.route("/api/hello2", methods=['GET'])
+def hello_visitor2():
+    """
+    This is to specifically test usage of `ProxyFix` configuration
+    to achieve dynamic IP detection when using `request.remote_addr` 
+    in production; which means `Flask` is behind a proxy server
+    """
+    # Get Visitor's IP Address
+    if os.environ.get('APP_IN_PRODUCTION'):
+        from werkzeug.middleware.proxy_fix import ProxyFix  # noqa
+
+        app.wsgi_app = ProxyFix(
+            app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+        )
+
+    return get_json_result(request, request.remote_addr)
 
 
 if __name__ == "__main__":
